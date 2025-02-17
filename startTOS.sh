18737e3e25f37d11ceece52fc260cde91ee0b5a5
Paul-Anthony-Martinez <pablo14pamg@gmail.com>
2025-02-17 02:08:21 -0500

    Create startTOS.sh
    TempleOS VM start up script.

---
+++ startTOS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Where the disk iamge was previously created
+hda=/home/anthonyslx/src/rep/templeos/vm/toshd.img
+# Options to start up the virtual machine
+options=" -rtc base=localtime -enable-kvm -display sdl,gl=on -vga virtio -machine pcspk-audiodev=snd0 -audiodev pa,id=snd0 -m 1024 -smp $(nproc) -cdrom ./TempleOS.ISO"
+
+qemu-system-x86_64 $options $hda
