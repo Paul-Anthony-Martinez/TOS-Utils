fd366061c10d3f32b26ba7a3af8d75e9d8a049e4
Paul-Anthony-Martinez <pablo14pamg@gmail.com>
2025-02-17 02:08:41 -0500

    Update startTOS.sh
    Fixed "image" typo.

--- startTOS.sh
+++ startTOS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Where the disk iamge was previously created
+# Where the disk image was previously created
 hda=/home/anthonyslx/src/rep/templeos/vm/toshd.img
 # Options to start up the virtual machine
 options=" -rtc base=localtime -enable-kvm -display sdl,gl=on -vga virtio -machine pcspk-audiodev=snd0 -audiodev pa,id=snd0 -m 1024 -smp $(nproc) -cdrom ./TempleOS.ISO"
